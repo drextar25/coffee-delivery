a3b995b246caf0c0795929dd514a07110c089d60
Andressa K Barbosa Oliveira <108102331+drextar25@users.noreply.github.com>
2023-04-17 10:25:45 -0400

    Update margin for mobile

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -2,6 +2,10 @@ import styled from "styled-components";
 
 export const HomeContainer = styled.div`
     margin-bottom: 9.8rem;
+    
+    @media(max-width:400px){
+    marin-top:4rem;
+    }
 `;
 
 
